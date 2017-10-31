75c8407f1d4b33fc8fd105b340ee56930f9f1492
刘齐轩 <qixuan.lqx@hotmail.com>
2017-10-31 18:30:41 +0800

    添加本地数据库

--- MVCiHealth/Controllers/AccountController.cs
+++ MVCiHealth/Controllers/AccountController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCiHealth.Models;
 
 namespace MVCiHealth.Controllers
 {
     public class AccountController : Controller
     {
+        Models.LocalDBEntities db = new LocalDBEntities();
         // GET: Account
         public ActionResult Login()
         {
+            var m = db.USERINFO.First();
             Global.IsLoggedIn = true;
-            return View();
+            return View(m);
         }
         // GET: Account
         public ActionResult Logout()
