e34e339a260e0f1d3c4f7f385a615c2a4e3cbdb1
刘齐轩 <qixuan.lqx@hotmail.com>
2017-11-28 16:28:31 +0800

    Revert "修改登录页样式"
    This reverts commit a60d8cbac4701c0aa4a37fa538f67582e3cf09bd.

--- MVCiHealth/Controllers/AccountController.cs
+++ MVCiHealth/Controllers/AccountController.cs
@@ -14,21 +14,10 @@ namespace MVCiHealth.Controllers
         public ActionResult Login()
         {
             //GET自动登陆
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Login(USERINFO u)
-        {
-            //GET自动登陆
-            var m = db.USERINFO.Find(u.USER_ID);
-            if (m.USER_PW == u.USER_PW)
-                Global.IsLoggedIn = true;
-            else
-                Global.IsLoggedIn = false;
+            var m = db.USERINFO.First();
+            Global.IsLoggedIn = true;
             return View(m);
         }
-
         // GET: Account
         public ActionResult Logout()
         {
