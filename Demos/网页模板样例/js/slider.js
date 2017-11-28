4307fa8b28d5cbe795179fd99894005dbaf4e2cc
刘齐轩 <qixuan.lqx@hotmail.com>
2017-11-28 17:56:49 +0800

    完成数据表，下一步做数据视图

---
+++ Demos/网页模板样例/js/slider.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+
+		$(function(){
+			$('#slides').slides({
+				preload: true,
+				preloadImage: 'images/loading.gif',
+				play: 5000,
+				pause: 2500,
+				hoverPause: true,
+				animationStart: function(current){
+					$('.caption').animate({
+						bottom:-35
+					},100);
+					if (window.console && console.log) {
+						// example return of current slide number
+						console.log('animationStart on slide: ', current);
+					};
+				},
+				animationComplete: function(current){
+					$('.caption').animate({
+						bottom:0
+					},200);
+					if (window.console && console.log) {
+						// example return of current slide number
+						console.log('animationComplete on slide: ', current);
+					};
+				},
+				slidesLoaded: function() {
+					$('.caption').animate({
+						bottom:0
+					},200);
+				}
+			});
+		});
--- MVCiHealth/Controllers/AccountController.cs
+++ MVCiHealth/Controllers/AccountController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCiHealth.Models;
 
 namespace MVCiHealth.Controllers
 {
     public class AccountController : Controller
     {
+        Models.LocalDBEntities db = new LocalDBEntities();
         // GET: Account
         public ActionResult Login()
         {
+            //GET自动登陆
+            var m = db.USERINFO.First();
             Global.IsLoggedIn = true;
-            return View();
+            return View(m);
         }
         // GET: Account
         public ActionResult Logout()
