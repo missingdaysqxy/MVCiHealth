3963dd9165044892a0113070e6dfa8b49ad557a5
jasonplus1994 <33078080+jasonplus1994@users.noreply.github.com>
2017-12-06 21:07:22 +0800

    上传病历view

---
+++ MVCiHealth/Controllers/HistoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCiHealth.Controllers
+{
+    public class HistoryController : Controller
+    {
+        // GET: History
+        public ActionResult PatientView()
+        {
+            return View();
+        }
+        public ActionResult DoctorView()
+        {
+            return View();
+        }
+    }
+}
