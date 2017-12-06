a8b1e9dd443ecfb04257cedaa50fcb907e67c7e1
雨晨 孙 <411968435@qq.com>
2017-12-06 21:56:19 +0800

    patient的view

--- MVCiHealth/Controllers/PatientController.cs
+++ MVCiHealth/Controllers/PatientController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCiHealth.Models;
 
 namespace MVCiHealth.Controllers
 {
@@ -11,7 +12,46 @@ namespace MVCiHealth.Controllers
         // GET: Patient
         public ActionResult Index()
         {
-            return View();
+            var p = new PATIENT()
+            {
+                NAME = "TestUser",
+                BIRTH = DateTime.Parse("2014-02-28"),
+                GENDER = "男",
+                TEL = "021-62233333",
+                TEL2 = "13823333333",
+                EMAIL = "administrator@cs.ecnu.edu.cn",
+                ADDRESS = "上海市普陀区中山北路3663号",
+                BLOOD_TYPE = "O",
+                ALLERGIC_HISTORY = "无",
+                GENETIC_HISTORY = "无",
+                CAPITAL_OPERATION = "无",
+                EMERGENCY_NAME = "Tony",
+                EMERGENCY_TEL = "13852333333",
+                COMMENT = "呵呵"
+            };
+            return View(p);
+        }
+
+        public ActionResult EditInfo()
+        {
+            var p = new PATIENT()
+            {
+                NAME = "TestUser",
+                BIRTH = DateTime.Parse("2014-02-28"),
+                GENDER = "男",
+                TEL = "021-62233333",
+                TEL2 = "13823333333",
+                EMAIL = "administrator@cs.ecnu.edu.cn",
+                ADDRESS = "上海市普陀区中山北路3663号",
+                BLOOD_TYPE = "O",
+                ALLERGIC_HISTORY = "无",
+                GENETIC_HISTORY = "无",
+                CAPITAL_OPERATION = "无",
+                EMERGENCY_NAME = "Tony",
+                EMERGENCY_TEL = "13852333333",
+                COMMENT = "呵呵"
+            };
+            return View(p);
         }
     }
 }
