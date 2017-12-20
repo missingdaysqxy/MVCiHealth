f11aeec234ea5046a2e97f57b4f56fc93b634adc
Leo Liu <leo.liu.926@outlook.com>
2017-12-20 19:25:01 +0800

    liuyu12/20

--- MVCiHealth/Controllers/HomeController.cs
+++ MVCiHealth/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVCiHealth.Models;
@@ -9,27 +12,45 @@ namespace MVCiHealth.Controllers
 {
     public class HomeController : Controller
     {
+        iHealthEntities db = new iHealthEntities();
         public ActionResult Index()
         {
             //向本地数据库中添加示范数据
-            MVCiHealth.Utils.SampleDataFiller.FillData();
-            return View();
+            //MVCiHealth.Utils.SampleDataFiller.FillData();
+
+            var home = new Home();
+            home.V_EVALUATIONLIST = new List<V_EVALUATION>();
+            home.USERINFOLIST = new List<USERINFO>();
+            home.DOCTORLIST = db.DOCTOR.OrderByDescending(m => m.LEVEL).Take(3).ToList();
+            
+
+            foreach (var i in home.DOCTORLIST)
+            {
+                int doc_id = i.DOCTOR_ID;
+                var for_test = db.V_EVALUATION.Where(m => m.RATE >= 3 && m.DOCTOR_ID == doc_id).First();
+                home.V_EVALUATIONLIST.Add(for_test);
+            }
+            return View(home);
         }
 
         [ActionName("_FirstPart")]
         public PartialViewResult FirstPart()
         {
-            ViewBag.PartTitle = "Description of iHealth";
+
+            ViewBag.PATIENTNUM = db.USERINFO.Count(m => m.GROUP_ID == 1);
+            ViewBag.DOCTORNUM = db.USERINFO.Count(m => m.GROUP_ID == 2);
             return PartialView();
         }
         [ActionName("_SecondPart")]
         public PartialViewResult SecondPart()
         {
+            
             ViewBag.PartTitle = "Requirements Introduction";
             return PartialView();
         }
         public PartialViewResult ThirdPart()
         {
+
             return PartialView();
         }
 
@@ -47,4 +68,5 @@ namespace MVCiHealth.Controllers
             return View();
         }
     }
+    
 }
---
+++ MVCiHealth/Models/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCiHealth.Models
+{
+    public class Home
+    {
+        public IList<DOCTOR> DOCTORLIST { get; set; }
+        public IList<V_EVALUATION> V_EVALUATIONLIST { get; set; }
+        public IList<USERINFO> USERINFOLIST { get; set; }
+    }
+}
